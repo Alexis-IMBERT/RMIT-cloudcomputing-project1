5bd240a5818edf825dbd6843107ec7cd464b5ea3
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2023-03-08 16:13:46 +1100

    working site

--- s3989748/config.py
+++ s3989748/config.py
@@ -1,4 +1,4 @@
 # To generate a new secret key:
 # >>> import random, string
 # >>> "".join([random.choice(string.printable) for _ in range(24)])
-SECRET_KEY = "#d#JCqTTW\nilK\\7m\x0bp#\tj~#H"
+SECRET_KEY = "\x0b<3R3O+f!PU-O Xi;J9n;:\\"
--- s3989748/views.py
+++ s3989748/views.py
@@ -1,14 +1,13 @@
 from flask import Flask
 
-# Config options - Make sure you created a 'config.py' file.
-app.config.from_object('config')
-# To get one variable, tape app.config['MY_VARIABLE']
 
 app = Flask(__name__)
+
+# app.config.from_object('config.py')
 
 @app.route('/')
 def index():
     return "Hello world !"
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
