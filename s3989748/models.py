18aa0f7ecf4074b665174862cc390e2c5d95418f
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2023-03-08 16:50:08 +1100

    add models file

---
+++ s3989748/models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+from .views import app
+
+# Create database connection object
+db = SQLAlchemy(app)
+
+class Login(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, emial, username, password):
+        self.email    = email
+        self.username = username
+        self.password = password
+
+db.create_all()
