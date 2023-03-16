0277df75ecf6bbaa0e5a6f46991b45bf196d6b6a
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2023-03-16 11:33:51 +1100

    fonctionnement de la BD

--- s3989748/__init__.py
+++ s3989748/__init__.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 
 from .views import app
 from . import models
+from .models import db
 
 # Connect sqlalchemy to app
-models.db.init_app(app)
+# models.db.init_app(app)
+
 
 @app.cli.command()
 def init_db():
--- s3989748/models.py
+++ s3989748/models.py
@@ -7,21 +7,25 @@ from .views import app
 db = SQLAlchemy(app)
 
 class Login(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), nullable=False)
+    # id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=False, primary_key=True)
     username = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
+    # num_id = 0
+    
     def __init__(self, email, username, password):
+        # Login.num_id += 1
+        # self.id       = Login.num_id
         self.email    = email
         self.username = username
         self.password = password
 
-db.create_all()
 
-def init_db():
-    db.drop_all()
-    db.create_all()
-    db.session.add(Login("s39897480@student.rmit.edu.ai","AlexisIMBERT0","AlexisIMBERT0"))
-    db.session.commit()
-    lg.warning('Database initialized!')
+def init_db(): 
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        db.session.add(Login("s39897480@student.rmit.edu.au","AlexisIMBERT0","AlexisIMBERT0"))
+        db.session.commit()
+        lg.warning('Database initialized!')
