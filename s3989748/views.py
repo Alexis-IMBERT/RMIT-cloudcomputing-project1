a70a7585782ae881a565e5dfb35ed31b7e52234a
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2023-03-24 18:50:31 +1100

    utilisation de l'hérédité

--- s3989748/views.py
+++ s3989748/views.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index/')
 def index():
     return "Hello world !"
+
+@app.route('/login')
+def login():
+    return render_template('login.html', is_connected=False)
+
+@app.route('/register')
+def register():
+    return render_template('register.html', is_connected=False)
+
+@app.route('/home')
+def home():
+    return redirect('/')
 
 app.config.from_object('config')
 
