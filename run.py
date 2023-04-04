c4aada14e995cc04fddb4f48e7e90d9976ee4d66
Alexis IMBERT <alexis.imbert@insa-rouen.fr>
2023-04-04 10:22:10 +1000

    possibility to put in production

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
+""" Code to run the project """
 #! /usr/bin/env python
 from s3989748 import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=443, host='0.0.0.0')
